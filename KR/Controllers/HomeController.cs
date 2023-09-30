05d1d351758f9196c1f1f53f85856faae95534e9
mak7on22 <mur-mur-0998@mail.ru>
2023-09-30 18:02:25 +0600

    Added User Controller and add Atribute

---
+++ KR/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using KR.Models;
+using KR.Models.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace KR.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly BookStoragePortalContext _context;
+
+        public HomeController(ILogger<HomeController> logger, BookStoragePortalContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int pg = 1, Sort sortState = Sort.DateCreateAsc)
+        {
+            IQueryable<Book> products = _context.Books.AsQueryable();
+            switch (sortState)
+            {
+                case Sort.DateCreateAsc: products = products.OrderBy(p => p.DateAdd); break;
+                case Sort.DateCreateDesc: products = products.OrderByDescending(p => p.DateAdd); break;
+            }
+            const int pageSize = 2;
+            if (pg < 1)
+                pg = 1;
+            int recsCount = products.Count();
+            var pager = new Pager(recsCount, pg, pageSize);
+            int recSkip = (pg - 1) * pageSize;
+            var data = await products.Skip(recSkip)
+                .Take(pageSize)
+                .ToListAsync();
+            this.ViewBag.Pager = pager;
+            ViewBag.DataCteateSort = sortState == Sort.DateCreateAsc ? Sort.DateCreateDesc : Sort.DateCreateAsc;
+            return View(data);
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ KR/Controllers/UsersController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KR.Models;
+using Microsoft.AspNetCore.Authorization;
+using KR.Models.State;
+
+namespace KR.Controllers
+{
+    [Authorize(Roles = "User,Admin")]
+    public class UsersController : Controller
+    {
+        private readonly BookStoragePortalContext _context;
+
+        public UsersController(BookStoragePortalContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int Id)
+        {
+            User user = await _context.Users.FirstOrDefaultAsync();
+
+            if (user != null)
+            {
+                Book selectedBook = await _context.Books.FindAsync(Id);
+
+                    if (user.Books == null)
+                    {
+                        user.Books = new List<Book>();
+                    }
+                    States(selectedBook);
+                    user.Books.Add(selectedBook);
+                    await _context.SaveChangesAsync();
+
+                return View(user);
+            }
+            else
+            {
+                return Problem("No users found.");
+            }
+        }
+        public void States(Book book)
+        {
+            if (book.State == "В наличии")
+                book.BookState = new BookInStockState();
+            else
+                book.BookState = new TakenBookState();
+        }
+
+        public async Task<IActionResult> LCabinet(int Id)
+        {
+            User user = await _context.Users.FirstOrDefaultAsync();
+
+            if (user != null)
+            {
+                Book selectedBook = await _context.Books.FindAsync(Id);
+
+                if (selectedBook != null)
+                {
+                    if (user.Books == null)
+                    {
+                        user.Books = new List<Book>();
+                    }
+                    States(selectedBook);
+                    user.Books.Add(selectedBook);
+                    await _context.SaveChangesAsync();
+                }
+                else
+                {
+                    return Problem("Selected book not found.");
+                }
+                return View(user); 
+            }
+            else
+            {
+                return Problem("No user found.");
+            }
+        }
+
+        public async Task<IActionResult> LCabinets()
+        {
+
+            User user = await _context.Users.FirstOrDefaultAsync();
+
+            if (user != null)
+            {
+
+                return View(user);
+            }
+            else
+            {
+                return Problem("No user found.");
+            }
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Surname,Email,ContactPhone")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        private bool WorkerExists(int id)
+        {
+            return (_context.Users?.Any(e => e.UserId == id)).GetValueOrDefault();
+        }
+    }
+}
+
