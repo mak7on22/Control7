9e7b1e36eb4c08675c3f5490ec707871e94eee64
mak7on22 <mur-mur-0998@mail.ru>
2023-09-30 18:00:57 +0600

    Added BooksController and add logic property

--- KR/Controllers/BooksController.cs
+++ KR/Controllers/BooksController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace KR.Controllers
 {
-    [Authorize(Roles = "User,Admin")]
+    
     public class BooksController : Controller
     {
         private readonly BookStoragePortalContext _context;
@@ -21,7 +21,6 @@ namespace KR.Controllers
             _context = context;
         }
 
-        // GET: Books
         public async Task<IActionResult> Index()
         {
               return _context.Books != null ? 
@@ -47,7 +46,7 @@ namespace KR.Controllers
             return View(book);
         }
 
-        // GET: Books/Create
+        [Authorize(Roles = "User,Admin")]
         public IActionResult Create()
         {
             return View();
@@ -61,12 +60,12 @@ namespace KR.Controllers
         {
             book.State = "В наличии";
             States(book);
-            book.DateAdd = DateTime.Now.Date;
+            book.DateAdd = DateTime.Now;
             if (ModelState.IsValid)
             {
                 _context.Add(book);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Index", "Home");
             }
             return View(book);
         }
@@ -77,94 +76,9 @@ namespace KR.Controllers
             else
                 book.BookState = new TakenBookState();
         }
-        // GET: Books/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Books == null)
-            {
-                return NotFound();
-            }
 
-            var book = await _context.Books.FindAsync(id);
-            if (book == null)
-            {
-                return NotFound();
-            }
-            return View(book);
-        }
 
-        // POST: Books/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Author,ImgUrl,YearOfRelease,Description,DateAdd,State")] Book book)
-        {
-            if (id != book.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(book);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!BookExists(book.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(book);
-        }
-
-        // GET: Books/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Books == null)
-            {
-                return NotFound();
-            }
-
-            var book = await _context.Books
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (book == null)
-            {
-                return NotFound();
-            }
-
-            return View(book);
-        }
-
-        // POST: Books/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Books == null)
-            {
-                return Problem("Entity set 'BookStoragePortalContext.Books'  is null.");
-            }
-            var book = await _context.Books.FindAsync(id);
-            if (book != null)
-            {
-                _context.Books.Remove(book);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
+        [Authorize(Roles = "User,Admin")]
         private bool BookExists(int id)
         {
           return (_context.Books?.Any(e => e.Id == id)).GetValueOrDefault();
